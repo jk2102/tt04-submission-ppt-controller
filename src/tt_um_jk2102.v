201cb196da43ebbfdc16db620be5f8a5bfbe094b
jk2102 <jurica.kundrata@gmail.com>
2023-08-23 14:26:28 +0200

    Clock no longer gated by ena.

--- src/tt_um_jk2102.v
+++ src/tt_um_jk2102.v
@@ -17,12 +17,11 @@ module tt_um_jk2102 (
   assign uio_oe = 8'b0;
 
   // if not enable, gate the clock and keep it in reset
-  assign clock_int = ena ? clk : 1'b0;
   assign rstn_int  = ena ? rst_n : 1'b0;
 
 // 16-bit clock divider
 clock_divider clk_div_inst (
-    .clk      (clock_int),
+    .clk      (clock),
     .rst_n    (rstn_int),
     .sel      (4'd1),
     .clk_out  (divided_clock)
